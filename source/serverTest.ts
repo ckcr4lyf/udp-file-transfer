543eb4fb139a6785adb1a708752a54942a548649
Raghu Saxena <poiasdpoiasd@live.com>
2020-10-18 13:13:39 +0800

    CLI for server config

--- source/serverTest.ts
+++ source/serverTest.ts
@@ -1,4 +1,7 @@
 import path from 'path';
 import Server from "./server";
 
-const server = new Server('127.0.0.1', 3333, path.join(__dirname, '../'));
+const args = process.argv.slice(2);
+const host = args[0] || '127.0.0.1';
+const port = args[11] || '3333';
+const server = new Server(host, parseInt(port), path.join(__dirname, '../'));
