b914c8aca4fc820190e558e25e89c77adb285194
Raghu Saxena <poiasdpoiasd@live.com>
2020-10-18 13:16:32 +0800

    Fix index

--- source/serverTest.ts
+++ source/serverTest.ts
@@ -3,5 +3,5 @@ import Server from "./server";
 
 const args = process.argv.slice(2);
 const host = args[0] || '127.0.0.1';
-const port = args[11] || '3333';
+const port = args[1] || '3333';
 const server = new Server(host, parseInt(port), path.join(__dirname, '../'));
